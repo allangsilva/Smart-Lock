4106c329fd850a2417a15b073f19b98e0ec522d3
Allan Galdino <agaldino12333@gmail.com>
2020-08-23 11:19:12 -0300

    starting the jovo project

---
+++ allan-galdino-smart-lock/test/launch.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+
+jest.setTimeout(500);
+
+for (const p of [new Alexa(), new GoogleAssistant()]) {
+  const testSuite = p.makeTestSuite();
+
+  describe(`PLATFORM: ${p.constructor.name} INTENTS`, () => {
+    test('should return a welcome message and ask for the name at "LAUNCH"', async () => {
+      const conversation = testSuite.conversation();
+
+      const launchRequest = await testSuite.requestBuilder.launch();
+      const response = await conversation.send(launchRequest);
+
+      expect(response.isAsk(expectedLaunchPrompt, expectedLaunchReprompt)).toBeTruthy();
+    });
+  });
+}
+
+let expectedLaunchPrompt = 'Seja Bem-Vindo! Você gostaria destrancar a porta? Checar o status? Ou trancar a porta?';
+let expectedLaunchReprompt = 'Por favor, diga trancar porta, checar status da minha porta, ou destravar para controlar sua porta.';
