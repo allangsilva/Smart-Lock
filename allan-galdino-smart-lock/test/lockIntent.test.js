4b48b024ab9f6caa996a29098c25030243f14b89
Allan Galdino <agaldino12333@gmail.com>
2020-08-23 12:22:17 -0300

    I added lockIntent unit test for lock response status

---
+++ allan-galdino-smart-lock/test/lockIntent.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+
+jest.setTimeout(500);
+
+for (const p of [new Alexa(), new GoogleAssistant()]) {
+  const testSuite = p.makeTestSuite();
+
+  describe(`PLATFORM: ${p.constructor.name} INTENTS`, () => {
+    test('should return LOCKED status at "LockIntent"', async () => {
+      const conversation = testSuite.conversation();
+
+      const lockRequest = await testSuite.requestBuilder.intent('LockIntent', { lockStatus: 'lock' });
+      const response = await conversation.send(lockRequest);
+
+      expect(response.isTell(expectedLockPrompt).toBeTruthy());
+    });
+  });
+
+  describe(`PLATFORM: ${p.constructor.name} INTENTS`, () => {
+    test('should return UNLOCKED status at "LockIntent"', async () => {
+      const conversation = testSuite.conversation();
+
+      const lockRequest = await testSuite.requestBuilder.intent('LockIntent', { lockStatus: 'unlock' });
+      const response = await conversation.send(lockRequest);
+
+      expect(response.isTell(expectedUnlockPrompt).toBeTruthy());
+    });
+  });
+}
+
+let expectedLockPrompt = 'Porta trancada!';
+let expectedUnlockPrompt = 'Porta destrancada!';
